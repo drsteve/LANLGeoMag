954407636030b7d00fa1e4efcfe9d5c1cdcb266c
Brian Larsen <balarsen@lanl.gov>
2011-01-11 16:33:57 -0700

    added setup.py so that Python_Lgm will compile and install

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Python_Lgm',
+      version='0.0',
+      description='Python wrapper for Lgm',
+      author='Brian Larsen',
+      author_email='balarsen@lanl.gov',
+      packages=['.']
+     )
