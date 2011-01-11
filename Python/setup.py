07de2f84ad1e40da848525bc38e72dc0b37c33c8
balarsen <balarsen@608d7d18-a564-11df-a798-f9e52c1bc2ef>
2011-01-11 23:39:26 +0000

    added setup.py so that lgmpy will install
    git-svn-id: svn+ssh://svn.isr.lanl.gov/n/svn/LanlGeoMag/trunk@127 608d7d18-a564-11df-a798-f9e52c1bc2ef

---
+++ Python/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='lgmpy',
+      version='0.0',
+      description='Python wrapper for Lgm',
+      author='Brian Larsen',
+      author_email='balarsen@lanl.gov',
+      packages=['.']
+     )
