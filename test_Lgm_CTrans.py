cab602e74a17bb601af3a4f9486754f3355c3e58
Brian Larsen <balarsen@lanl.gov>
2011-01-13 18:00:01 -0700

    removing unneeded files and making more tests work

--- test_Lgm_CTrans.py
+++ test_Lgm_CTrans.py
@@ -16,10 +16,11 @@ import datetime
 
 import numpy
 
-import Lgm
 import Lgm_CTrans
 import Lgm_Vector
-from _Lgm import lib
+from Lgm_Wrap import Lgm_LeapSeconds, size_Lgm_LeapSeconds, size_CTrans, \
+    Lgm_DateTime, size_DateTime, Lgm_Set_Coord_Transforms, Lgm_Convert_Coords, \
+    GSM_TO_SM, SM_TO_GSM
 
 class Lgm_CTransTests(unittest.TestCase):
     """
@@ -39,38 +40,17 @@ class Lgm_CTransTests(unittest.TestCase):
 
     def test_sizeLgm_CTrans(self):
         """Lgm_CTrans c and python must have same size"""
-        #print("c: %d" % (lib.size_CTrans()) )
-        self.assertEqual(ctypes.sizeof(Lgm_CTrans.Lgm_CTrans), lib.size_CTrans())
+        #print("c: %d" % (size_CTrans()) )
+        self.assertEqual(ctypes.sizeof(Lgm_CTrans.Lgm_CTrans), size_CTrans())
 
     def test_sizeLgm_LeapSeconds(self):
         """Lgm_LeapSeconds c/python must be same size"""
-        self.assertEqual(ctypes.sizeof(Lgm_CTrans.Lgm_LeapSeconds),
-                         lib.size_Lgm_LeapSeconds())
+        self.assertEqual(ctypes.sizeof(Lgm_LeapSeconds),
+                         size_Lgm_LeapSeconds())
 
     def test_sizeDateTime(self):
         """DateTime c and python should match size"""
-        self.assertEqual(ctypes.sizeof(Lgm_CTrans.Lgm_DateTime), lib.size_DateTime())
-
-    def test_Lgm_DateTime(self):
-        """Lgm_DateTime should create and set defaults"""
-        a = Lgm_CTrans.Lgm_DateTime()
-        self.assertEqual(a.nNutationTerms, 106)
-        self.assertEqual(a.DUT1, 0.0)
-        self.assertEqual(a.ddPsi, 0.0)
-
-    def test_Lgm_Convert_Coords(self):
-        """Lgm_Convert_Coords should give known output (regression)"""
-        # taken from CoordQuickStart.c
-        Date = 20040812
-        UTC = 12.34567
-        Ugsm = Lgm_Vector.Lgm_Vector(x = -6.6, y = 3.4, z = -2.3)
-        Usm = Lgm_Vector.Lgm_Vector()
-        c = Lgm_CTrans.Lgm_CTrans()
-        lib.Lgm_Set_Coord_Transforms(Date, UTC, c)
-        lib.Lgm_Convert_Coords(Ugsm, Usm, Lgm_CTrans.GSM_TO_SM, c)
-        self.assertAlmostEqual(-5.5352494753370127, Usm.x)
-        self.assertAlmostEqual( 3.3999999999999995, Usm.y)
-        self.assertAlmostEqual(-4.2674363786448328, Usm.z)
+        self.assertEqual(ctypes.sizeof(Lgm_DateTime), size_DateTime())
 
     def test_Lgm_Convert_CoordsBack(self):
         """Lgm_Convert_Coords should give known output (and back)"""
@@ -80,12 +60,13 @@ class Lgm_CTransTests(unittest.TestCase):
         Ugsm = Lgm_Vector.Lgm_Vector(x = -6.6, y = 3.4, z = -2.3)
         Usm = Lgm_Vector.Lgm_Vector()
         c = Lgm_CTrans.Lgm_CTrans()
-        lib.Lgm_Set_Coord_Transforms(Date, UTC, c)
-        lib.Lgm_Convert_Coords(Ugsm, Usm, Lgm_CTrans.GSM_TO_SM, c)
+        Lgm_Set_Coord_Transforms(Date, UTC, ctypes.pointer(c))
+        Lgm_Convert_Coords(ctypes.pointer(Ugsm), ctypes.pointer(Usm),
+                           GSM_TO_SM, ctypes.pointer(c))
         self.assertAlmostEqual(-5.5352494753370127, Usm.x)
         self.assertAlmostEqual( 3.3999999999999995, Usm.y)
         self.assertAlmostEqual(-4.2674363786448328, Usm.z)
-        lib.Lgm_Convert_Coords(Usm, Ugsm, Lgm_CTrans.SM_TO_GSM, c)
+        Lgm_Convert_Coords(Usm, Ugsm, SM_TO_GSM, c)
         self.assertAlmostEqual(-6.6, Ugsm.x)
         self.assertAlmostEqual( 3.4, Ugsm.y)
         self.assertAlmostEqual(-2.3, Ugsm.z)
@@ -116,6 +97,16 @@ class Lgm_CTransTests(unittest.TestCase):
         self.assertEqual([12.5, 12.5], Lgm_CTrans.dateToFPHours([d1, d1]))
         self.assertEqual([12.5, 12.5],
             Lgm_CTrans.dateToFPHours(numpy.array([d1, d1])).tolist())
+
+    def test_init_vals(self):
+        """__init__ sets upa few valiues (regression)"""
+        tst = Lgm_CTrans.Lgm_CTrans()
+        self.assertEqual(tst.nNutationTerms, 106)
+        self.assertEqual(tst.DUT1, 0.0)
+        self.assertEqual(tst.xp, 0.0)
+        self.assertEqual(tst.yp, 0.0)
+        self.assertEqual(tst.ddPsi, 0.0)
+        self.assertEqual(tst.ddEps, 0.0)
 
 
 if __name__ == '__main__':
--- test_Lgm_MagModelInfo.py
+++ test_Lgm_MagModelInfo.py
@@ -14,7 +14,9 @@ import ctypes
 import unittest
 
 import Lgm_MagModelInfo
-from Lgm_Wrap import size_MagModelInfo
+from Lgm_Wrap import size_MagModelInfo, TRUE, LGM_ABSOLUTE_JUMP_METHOD, DQAGS, \
+    DQAGP
+
 
 class Lgm_MagModelInfoTests(unittest.TestCase):
     """
@@ -36,6 +38,46 @@ class Lgm_MagModelInfoTests(unittest.TestCase):
         """for Lgm_MagModelInfo the c and python sizes should match"""
         self.assertEqual(size_MagModelInfo(),
                          ctypes.sizeof(Lgm_MagModelInfo.Lgm_MagModelInfo) )
+
+    def test_init_vals(self):
+        """test that the defaults in init don't change (regression)"""
+        mmi = Lgm_MagModelInfo.Lgm_MagModelInfo()
+        self.assertEqual(mmi.Kp, 5)
+        self.assertEqual(mmi.P, 2.1)
+        self.assertEqual(mmi.nFunc, 0)
+        self.assertEqual(mmi.B0, 1.00)
+        self.assertEqual(mmi.B1, 0.8100)
+        self.assertEqual(mmi.B2, 0.4065)
+        self.assertEqual(mmi.SavePoints, 0)
+        self.assertEqual(mmi.Hmax, 1.0)
+
+        self.assertEqual(mmi.UseInterpRoutines, TRUE)
+        self.assertEqual(mmi.Lgm_I_integrand_JumpMethod, LGM_ABSOLUTE_JUMP_METHOD)
+        #
+        #  Some inits for MagStep
+        #
+        self.assertEqual(mmi.Lgm_MagStep_FirstTimeThrough, TRUE)
+        self.assertEqual(mmi.Lgm_MagStep_eps_old, -1.0)
+        #
+        # Set some default tolerances
+        #
+        self.assertEqual(mmi.Lgm_MagFlux_Integrator_epsabs, 0.0)
+        self.assertEqual(mmi.Lgm_MagFlux_Integrator_epsrel, 1e-5)
+
+        self.assertEqual(mmi.Lgm_LambdaIntegral_Integrator_epsabs, 0.0)
+        self.assertEqual(mmi.Lgm_LambdaIntegral_Integrator_epsrel, 1e-3)
+
+        self.assertEqual(mmi.Lgm_I_Integrator_epsrel, 0.0)
+        self.assertEqual(mmi.Lgm_I_Integrator_epsabs, 1e-3)
+        self.assertEqual(mmi.Lgm_I_Integrator, DQAGS)
+
+        self.assertEqual(mmi.Lgm_Sb_Integrator_epsrel, 0.0)
+        self.assertEqual(mmi.Lgm_Sb_Integrator_epsabs, 1e-3)
+        self.assertEqual(mmi.Lgm_Sb_Integrator, DQAGP)
+
+        self.assertEqual(mmi.Lgm_FindBmRadius_Tol, 1e-10)
+        self.assertEqual(mmi.Lgm_FindShellLine_I_Tol, 1e-3)
+        self.assertEqual(mmi.Lgm_TraceToMirrorPoint_Tol, 1e-7)
 
 
 
