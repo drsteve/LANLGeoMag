d9225c455606a62b9ea495967fe602b45a4653e3
Brian Larsen <balarsen@lanl.gov>
2011-02-28 15:17:16 -0700

    fixed a floating point assertEqual on 2 lists with numpy.testing.assert_array_almost_equal

--- Python/test_Lgm_T89.py
+++ Python/test_Lgm_T89.py
@@ -41,7 +41,7 @@ class Lgm_T89_T89(unittest.TestCase):
 
     def test_T89(self):
         """the T89 simple static wrapper should work (regression)"""
-        self.assertEqual(Lgm_T89.T89(self.pos, self.dt, self.kp),
+        numpy.testing.assert_array_almost_equal( Lgm_T89.T89(self.pos, self.dt, self.kp),
             [-18.976439213243122, -1.8653978086481349, 80.39310505873112])
         ans = [[-18.976439213243122, -1.8653978086481349, 80.39310505873112]*2]
         val = Lgm_T89.T89([self.pos]*2, [self.dt]*2, [self.kp]*2)
