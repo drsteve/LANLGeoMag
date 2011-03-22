6f9c1b9c86629ea781979d405ed5d9ca16ecffbf
Steve Morley <smorley@lanl.gov>
2011-03-22 14:16:38 -0600

    fixing imports on import lgmpy

--- Python/lgmpy/__init__.py
+++ Python/lgmpy/__init__.py
@@ -9,3 +9,14 @@ See Mike Henderon about LanlGeoMag and Brian Larsen about the python wrappers
 
 @version: V1: 06-Dec-2010 (BAL)
 """
+# put modules here that you want to be accessible through 'from spacepy import *'
+__all__ = ['Lstar', 'Closed_Field', 'Lgm_T89', 'Lgm_OP77']
+
+# Expose definitions from modules in this package.
+from lgmpy import Closed_Field, Lgm_OP77, MagData, Lgm_T89, Lgm_CTrans, Lgm_MagEphemInfo
+from lgmpy import Lgm_Wrap, Lgm_MagModelInfo,  Lstar
+
+#package info
+__version__ = '0.1dev'
+__author__ = ['Brian Larsen', 'Steve Morley']
+
