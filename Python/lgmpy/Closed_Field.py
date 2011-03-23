drsteve/LANLGeoMag

commit a7d93e78ada32676c5aaadb903591de9d75dea50

added eccentric dipole - needs rebuild

Python/lgmpy/Closed_Field.py
@@ -14,6 +14,7 @@ from Lgm_Wrap import Lgm_Convert_Coords, GSM_TO_SM, WGS84_A
 import Lgm_Vector
 import Lgm_MagModelInfo
 import Lgm_CTrans
+from _Bfield_dict import Bfield_dict
 
 def _simpleL(position, MagModelInfo):
     """
@@ -62,13 +63,10 @@ def Closed_Field(*args, **kwargs):
             raise(NotImplementedError('Different coord systems are not yet ready to use') )
         # could consider a Lgm_MagModelInfo param to use an existing one
         mmi = Lgm_MagModelInfo.Lgm_MagModelInfo()
-        if kwargs['bfield'] == 'Lgm_B_OP77':
-            Lgm_Set_Lgm_B_OP77( pointer(mmi) )
-        elif kwargs['bfield'] == 'Lgm_B_T89':
-            mmi.Kp = kwargs['Kp']
-            Lgm_Set_Lgm_B_T89( pointer(mmi) )
-        else:
-            raise(NotImplementedError('Only Lgm_B_OP77 and Lgm_B_T89 implented so far'))
+        try:
+            Bfield_dict[kwargs['bfield']](MagEphemInfo.LstarInfo.contents.mInfo)
+        except KeyError:
+            raise(NotImplementedError("Only Bfield='OP77, T89' currently supported"))
 
         datelong = Lgm_CTrans.dateToDateLong(args[1])
         utc = Lgm_CTrans.dateToFPHours(args[1])

Python/LIN_Lgm_Wrap.py.bak
@@ -2850,36 +2850,42 @@ if hasattr(_libs['libLanlGeoMag.so'], 'Lgm_Set_Lgm_B_cdip'):
     Lgm_Set_Lgm_B_cdip.restype = None
 
 # /usr/local/include/Lgm/Lgm_MagModelInfo.h: 496
+if hasattr(_libs['libLanlGeoMag.so'], 'Lgm_Set_Lgm_B_edip'):
+    Lgm_Set_Lgm_B_edip = _libs['libLanlGeoMag.so'].Lgm_Set_Lgm_B_edip
+    Lgm_Set_Lgm_B_edip.argtypes = [POINTER(Lgm_MagModelInfo)]
+    Lgm_Set_Lgm_B_edip.restype = None
+
+# /usr/local/include/Lgm/Lgm_MagModelInfo.h: 497
 if hasattr(_libs['libLanlGeoMag.so'], 'Lgm_Set_Lgm_B_igrf'):
     Lgm_Set_Lgm_B_igrf = _libs['libLanlGeoMag.so'].Lgm_Set_Lgm_B_igrf
     Lgm_Set_Lgm_B_igrf.argtypes = [POINTER(Lgm_MagModelInfo)]
     Lgm_Set_Lgm_B_igrf.restype = None
 
-# /usr/local/include/Lgm/Lgm_MagModelInfo.h: 497
+# /usr/local/include/Lgm/Lgm_MagModelInfo.h: 498
 if hasattr(_libs['libLanlGeoMag.so'], 'Lgm_Set_Lgm_B_T01S'):
     Lgm_Set_Lgm_B_T01S = _libs['libLanlGeoMag.so'].Lgm_Set_Lgm_B_T01S
     Lgm_Set_Lgm_B_T01S.argtypes = [POINTER(Lgm_MagModelInfo)]
     Lgm_Set_Lgm_B_T01S.restype = None
 
-# /usr/local/include/Lgm/Lgm_MagModelInfo.h: 498
+# /usr/local/include/Lgm/Lgm_MagModelInfo.h: 499
 if hasattr(_libs['libLanlGeoMag.so'], 'Lgm_Set_gm_B_TS04'):
     Lgm_Set_gm_B_TS04 = _libs['libLanlGeoMag.so'].Lgm_Set_gm_B_TS04
     Lgm_Set_gm_B_TS04.argtypes = [POINTER(Lgm_MagModelInfo)]
     Lgm_Set_gm_B_TS04.restype = None
 
-# /usr/local/include/Lgm/Lgm_MagModelInfo.h: 499
+# /usr/local/include/Lgm/Lgm_MagModelInfo.h: 500
 if hasattr(_libs['libLanlGeoMag.so'], 'Lgm_Set_Lgm_B_T89'):
     Lgm_Set_Lgm_B_T89 = _libs['libLanlGeoMag.so'].Lgm_Set_Lgm_B_T89
     Lgm_Set_Lgm_B_T89.argtypes = [POINTER(Lgm_MagModelInfo)]
     Lgm_Set_Lgm_B_T89.restype = None
 
-# /usr/local/include/Lgm/Lgm_MagModelInfo.h: 500
+# /usr/local/include/Lgm/Lgm_MagModelInfo.h: 501
 if hasattr(_libs['libLanlGeoMag.so'], 'Lgm_Set_Lgm_B_OP77'):
     Lgm_Set_Lgm_B_OP77 = _libs['libLanlGeoMag.so'].Lgm_Set_Lgm_B_OP77
     Lgm_Set_Lgm_B_OP77.argtypes = [POINTER(Lgm_MagModelInfo)]
     Lgm_Set_Lgm_B_OP77.restype = None
 
-# /usr/local/include/Lgm/Lgm_MagModelInfo.h: 507
+# /usr/local/include/Lgm/Lgm_MagModelInfo.h: 508
 if hasattr(_libs['libLanlGeoMag.so'], 'Lgm_CDMAG_TO_CGM'):
     Lgm_CDMAG_TO_CGM = _libs['libLanlGeoMag.so'].Lgm_CDMAG_TO_CGM
     Lgm_CDMAG_TO_CGM.argtypes = [POINTER(Lgm_Vector), POINTER(c_double), POINTER(c_double), POINTER(c_double), POINTER(Lgm_MagModelInfo)]

Python/OSX_Lgm_Wrap.py.bak
@@ -3499,6 +3499,15 @@ for _lib in _libs.itervalues():
 
 # /usr/local/include/Lgm/Lgm_MagModelInfo.h: 496
 for _lib in _libs.itervalues():
+    if not hasattr(_lib, 'Lgm_Set_Lgm_B_edip'):
+        continue
+    Lgm_Set_Lgm_B_edip = _lib.Lgm_Set_Lgm_B_edip
+    Lgm_Set_Lgm_B_edip.argtypes = [POINTER(Lgm_MagModelInfo)]
+    Lgm_Set_Lgm_B_edip.restype = None
+    break
+
+# /usr/local/include/Lgm/Lgm_MagModelInfo.h: 497
+for _lib in _libs.itervalues():
     if not hasattr(_lib, 'Lgm_Set_Lgm_B_igrf'):
         continue
     Lgm_Set_Lgm_B_igrf = _lib.Lgm_Set_Lgm_B_igrf
@@ -3506,7 +3515,7 @@ for _lib in _libs.itervalues():
     Lgm_Set_Lgm_B_igrf.restype = None
     break
 
-# /usr/local/include/Lgm/Lgm_MagModelInfo.h: 497
+# /usr/local/include/Lgm/Lgm_MagModelInfo.h: 498
 for _lib in _libs.itervalues():
     if not hasattr(_lib, 'Lgm_Set_Lgm_B_T01S'):
         continue
@@ -3515,7 +3524,7 @@ for _lib in _libs.itervalues():
     Lgm_Set_Lgm_B_T01S.restype = None
     break
 
-# /usr/local/include/Lgm/Lgm_MagModelInfo.h: 498
+# /usr/local/include/Lgm/Lgm_MagModelInfo.h: 499
 for _lib in _libs.itervalues():
     if not hasattr(_lib, 'Lgm_Set_gm_B_TS04'):
         continue
@@ -3524,7 +3533,7 @@ for _lib in _libs.itervalues():
     Lgm_Set_gm_B_TS04.restype = None
     break
 
-# /usr/local/include/Lgm/Lgm_MagModelInfo.h: 499
+# /usr/local/include/Lgm/Lgm_MagModelInfo.h: 500
 for _lib in _libs.itervalues():
     if not hasattr(_lib, 'Lgm_Set_Lgm_B_T89'):
         continue
@@ -3533,7 +3542,7 @@ for _lib in _libs.itervalues():
     Lgm_Set_Lgm_B_T89.restype = None
     break
 
-# /usr/local/include/Lgm/Lgm_MagModelInfo.h: 500
+# /usr/local/include/Lgm/Lgm_MagModelInfo.h: 501
 for _lib in _libs.itervalues():
     if not hasattr(_lib, 'Lgm_Set_Lgm_B_OP77'):
         continue
@@ -3542,7 +3551,7 @@ for _lib in _libs.itervalues():
     Lgm_Set_Lgm_B_OP77.restype = None
     break
 
-# /usr/local/include/Lgm/Lgm_MagModelInfo.h: 507
+# /usr/local/include/Lgm/Lgm_MagModelInfo.h: 508
 for _lib in _libs.itervalues():
     if not hasattr(_lib, 'Lgm_CDMAG_TO_CGM'):
         continue

Python/lgmpy/Lstar.py
@@ -19,8 +19,7 @@ from spacepy import datamodel
 import spacepy.toolbox as tb
 
 from Lgm_Wrap import Lgm_Set_Coord_Transforms, SM_TO_GSM, Lgm_Convert_Coords, \
-    Lgm_Set_Lgm_B_OP77, SetLstarTolerances, \
-    RadPerDeg, Lgm_Set_Lgm_B_T89, \
+    SetLstarTolerances, RadPerDeg, \
     LFromIBmM_Hilton, LFromIBmM_McIlwain
 from Lgm_Wrap import Lstar as Lgm_Lstar
 import Lgm_Vector

Python/lgmpy/_Bfield_dict.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from Lgm_Wrap import Lgm_Set_Lgm_B_cdip, Lgm_Set_Lgm_B_OP77, Lgm_Set_Lgm_B_T89
+from Lgm_Wrap import Lgm_Set_Lgm_B_cdip, Lgm_Set_Lgm_B_edip, Lgm_Set_Lgm_B_OP77, Lgm_Set_Lgm_B_T89
 
 Bfield_dict = {'Lgm_B_cdip': Lgm_Set_Lgm_B_cdip,
+                'Lgm_B_edip': Lgm_Set_Lgm_B_edip,
                 'Lgm_B_OP77': Lgm_Set_Lgm_B_OP77,
                 'Lgm_B_T89': Lgm_Set_Lgm_B_T89,}

Python/lgmpy/magcoords.py
@@ -16,8 +16,9 @@ from ctypes import pointer, c_double
 import numpy
 from spacepy import datamodel
 
-from lgmpy.Lgm_Wrap import Lgm_Set_Coord_Transforms, Lgm_Convert_Coords, WGS84_TO_GSM, SM_TO_GSM, Lgm_McIlwain_L, Lgm_Set_Lgm_B_OP77, Lgm_Set_Lgm_B_T89
-from lgmpy import Lgm_Vector, Lgm_CTrans, Lgm_MagModelInfo
+from .Lgm_Wrap import Lgm_Set_Coord_Transforms, Lgm_Convert_Coords, WGS84_TO_GSM, SM_TO_GSM, Lgm_McIlwain_L
+from . import Lgm_Vector, Lgm_CTrans, Lgm_MagModelInfo
+from .Lstar import Lstar_Data
 from _Bfield_dict import Bfield_dict
 
 def LatLon2GSM(lat, lon, rad, time):
@@ -88,7 +89,6 @@ def Lvalue(*args, **kwargs):
     Iout = c_double()
     Bm = c_double()
     M = c_double()
-     
     ans = Lgm_McIlwain_L(datelong, utc, pointer(Pgsm), kwargs['alpha'], 
                             method_dict[kwargs['method']],
                             pointer(Iout), pointer(Bm), pointer(M),

libLanlGeoMag/Lgm/Lgm_MagModelInfo.h
@@ -493,6 +493,7 @@ void Lgm_MagModelInfo_Set_MagModel( Lgm_MagModelInfo *m, int InternalModel, int 
  * deal with setting
  */
 void Lgm_Set_Lgm_B_cdip(Lgm_MagModelInfo *MagInfo);
+void Lgm_Set_Lgm_B_edip(Lgm_MagModelInfo *MagInfo);
 void Lgm_Set_Lgm_B_igrf(Lgm_MagModelInfo *MagInfo);
 void Lgm_Set_Lgm_B_T01S(Lgm_MagModelInfo *MagInfo);
 void Lgm_Set_gm_B_TS04(Lgm_MagModelInfo *MagInfo);

libLanlGeoMag/Lgm_InitMagInfo.c
@@ -249,3 +249,8 @@ void Lgm_Set_Lgm_B_cdip(Lgm_MagModelInfo *MagInfo) {
     MagInfo->InternalModel = LGM_CDIP;
     MagInfo->Bfield = Lgm_B_cdip;
 }
+
+void Lgm_Set_Lgm_B_edip(Lgm_MagModelInfo *MagInfo) {
+    MagInfo->InternalModel = LGM_EDIP;
+    MagInfo->Bfield = Lgm_B_edip;
+}

commit cb5570b5a0c76601a2961d198e6d07a3b8cf3af3

fixed error introduced in a7d93e78ada32676c5aaadb903591de9d75dea50 where Kp was no longer passed through

Python/lgmpy/Closed_Field.py
@@ -63,10 +63,11 @@ def Closed_Field(*args, **kwargs):
             raise(NotImplementedError('Different coord systems are not yet ready to use') )
         # could consider a Lgm_MagModelInfo param to use an existing one
         mmi = Lgm_MagModelInfo.Lgm_MagModelInfo()
+        mmi.Kp = kwargs['Kp']
         try:
-            Bfield_dict[kwargs['bfield']](MagEphemInfo.LstarInfo.contents.mInfo)
+            Bfield_dict[kwargs['bfield']](pointer(mmi))
         except KeyError:
-            raise(NotImplementedError("Only Bfield='OP77, T89' currently supported"))
+            raise(NotImplementedError("Only Bfield=%s currently supported" % Bfield_dict.keys()))
 
         datelong = Lgm_CTrans.dateToDateLong(args[1])
         utc = Lgm_CTrans.dateToFPHours(args[1])