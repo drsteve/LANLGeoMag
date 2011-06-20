6e58a0b2b1bd41fb58710b62c1b19bafe5e3a61e
Brian Larsen <balarsen@lanl.gov>
2011-06-20 13:45:44 -0600

    changed so that ctypesgen is only run once. - not as pretty as it could be but seems to work

--- Python/make_Lgm_Wrap.py
+++ Python/make_Lgm_Wrap.py
@@ -24,26 +24,36 @@ headerpath = next((i[2:] for i in includes if i[-3:] == 'Lgm'))
 headers = glob.glob(os.path.join(headerpath, '*.h'))
 #Some includes in Lgm have the Lgm/, so include level above Lgm
 includes.extend((i[:-3] for i in includes if i[-3:] == 'Lgm' ))
+# go in and find the library and figure out its extension, either dylib or so
+# TODO are there more coices that need to be handled?
 
+tmp_name = []
+for pth in libpaths:
+    tmp_name.append(glob.glob(pth[2:] + '/' + 'libLanlGeoMag.*'))
+
+# we assume it is only in one place (it better be)
+while len(tmp_name) > 0:
+    libname = tmp_name.pop()
+    if len(libname) == 0:
+        continue
+
+while len(libname) > 0:
+    name = libname.pop()
+    if name.split(os.path.extsep)[-2] == '0':
+        continue
+    if name.split(os.path.extsep)[-1] == 'so':
+        library_name = name
+    elif name.split(os.path.extsep)[-1] == 'dylib':
+        library_name = name
 try:
     subprocess.call(['ctypesgen.py',
-                     '-llibLanlGeoMag.dylib',
+                     '-l' + library_name,
                      '--no-macro-warnings',
-                     '-o', 'OSX_Lgm_Wrap.py.bak'] +
-                    includes + libpaths + 
-                    headers)
-    subprocess.call(['ctypesgen.py',
-                     '-llibLanlGeoMag.so',
-                     '--no-macro-warnings',
-                     '-o', 'LIN_Lgm_Wrap.py.bak'] +
-                    includes + libpaths + 
+                     '-o', 'Lgm_Wrap.py.bak'] +
+                    includes + libpaths +
                     headers)
 except OSError:
-    print("ctyoesgen not installed, rename one of the *_Lgm_Wrap.py.bak files as Lgm_Wrap")
+    print("ctypesgen not installed, rename one of the *_Lgm_Wrap.py.bak files as Lgm_Wrap")
 finally:
-    if sys.platform == 'darwin':
-        shutil.copy('OSX_Lgm_Wrap.py.bak', 'lgmpy/Lgm_Wrap.py')
-    elif 'linux' in sys.platform:
-        shutil.copy('LIN_Lgm_Wrap.py.bak', 'lgmpy/Lgm_Wrap.py')
-    else:
-        raise(OSError("You are using an OS that I don't understand, sorry you are on your own"))
+    print('copying ' + 'Lgm_Wrap.py.bak' + ' to ' + 'lgmpy/Lgm_Wrap.py')
+    shutil.copy('Lgm_Wrap.py.bak', 'lgmpy/Lgm_Wrap.py')
