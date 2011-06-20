ac94c8ff5b627506b924f01d36ed7ef116c9103e
Jonathan Niehof <jniehof@lanl.gov>
2011-06-20 11:52:10 -0600

    Use pkg-config to find LanlGeoMag on python wrapping
     * Fall back to /usr/include/Lgm

--- Python/make_Lgm_Wrap.py
+++ Python/make_Lgm_Wrap.py
@@ -1,25 +1,43 @@
 #!/usr/bin/env python
 
+import glob
+import os.path
+import shutil
 import subprocess
 import sys
-import glob
-import shutil
 
-headers = glob.glob('/usr/local/include/Lgm/*.h')
+try:
+    p = subprocess.Popen(['pkg-config', '--cflags-only-I', 'lgm'],
+                         stdout=subprocess.PIPE)
+    output, errors = p.communicate()
+    assert p.returncode == 0
+    includes = output.split()
+    p = subprocess.Popen(['pkg-config', '--libs-only-L', 'lgm'],
+                         stdout=subprocess.PIPE)
+    output, errors = p.communicate()
+    assert p.returncode == 0
+    libpaths = output.split()
+except:
+    includes = ['-I/usr/local/include/Lgm']
+    libpaths = ['-L/usr/local/lib']
+headerpath = next((i[2:] for i in includes if i[-3:] == 'Lgm'))
+headers = glob.glob(os.path.join(headerpath, '*.h'))
+#Some includes in Lgm have the Lgm/, so include level above Lgm
+includes.extend((i[:-3] for i in includes if i[-3:] == 'Lgm' ))
 
 try:
     subprocess.call(['ctypesgen.py',
-                     '-L/usr/local/lib/',
                      '-llibLanlGeoMag.dylib',
                      '--no-macro-warnings',
                      '-o', 'OSX_Lgm_Wrap.py.bak'] +
-                     headers)
+                    includes + libpaths + 
+                    headers)
     subprocess.call(['ctypesgen.py',
-                     '-L/usr/local/lib/',
                      '-llibLanlGeoMag.so',
                      '--no-macro-warnings',
                      '-o', 'LIN_Lgm_Wrap.py.bak'] +
-                     headers)
+                    includes + libpaths + 
+                    headers)
 except OSError:
     print("ctyoesgen not installed, rename one of the *_Lgm_Wrap.py.bak files as Lgm_Wrap")
 finally:
