f6d3d2b63d52a35a67746a95c0da904a32b72e75
Mike Henderson <mghenderson@lanl.gov>
2011-08-11 16:54:54 -0600

    Added missing file...

---
+++ libLanlGeoMag/Lgm/Lgm_Misc.h
@@ -0,0 +1,11 @@
+#ifndef LGM_MISC_H
+#define LGM_MISC_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+void Lgm_ReplaceSubString( char *NewStr, char *OrigStr, char *SubStr, char *RepStr );
+
+
+#endif
