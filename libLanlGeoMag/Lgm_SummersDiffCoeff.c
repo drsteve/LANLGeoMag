366fb6bd3351ab4c5cb28555aaa476500bad1ee9
Jonathan Niehof <jniehof@lanl.gov>
2011-08-18 11:06:15 -0600

    Comment out debugging printf for every run through SummersIntegrand_Gaa
     * Per request of G. Cunningham

--- libLanlGeoMag/Lgm_SummersDiffCoeff.c
+++ libLanlGeoMag/Lgm_SummersDiffCoeff.c
@@ -448,7 +448,7 @@ double  SummersIntegrand_Gaa( double Lat, _qpInfo *qpInfo ) {
      * Finally the integrand.
      */
     f = Da0a0 * CosLat*v/sqrt(1-BoverBm);
-printf("%g %g\n", Lat*DegPerRad, f);
+    /*printf("%g %g\n", Lat*DegPerRad, f);*/
 
     return( f );
 
