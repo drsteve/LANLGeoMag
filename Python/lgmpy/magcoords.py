f042a662e1ca66600886c6a16641d837bd2fa7fa
Brian Larsen <balarsen@lanl.gov>
2012-07-27 10:22:50 -0600

    lgmpy: updated magcoords with a couple new coord transforms.  GSE_TO_GEI2000, GEI2000_TO_GSM, GEI2000_TO_GSE, GSM_TO_GEI2000

--- Python/lgmpy/magcoords.py
+++ Python/lgmpy/magcoords.py
@@ -18,7 +18,7 @@ from spacepy import datamodel
 
 
 from lgmpy.Lgm_Wrap import Lgm_Set_Coord_Transforms, Lgm_Convert_Coords, Lgm_McIlwain_L
-from Lgm_Wrap import TEME_TO_WGS84, WGS84_TO_GSM, GSM_TO_WGS84, WGS84_TO_GSE, GSE_TO_WGS84, SM_TO_GSM, GSM_TO_SM, GSM_TO_GSE, GSE_TO_GSM, MOD_TO_GSM, GSE_TO_SM, SM_TO_GSE, MOD_TO_GSE, MOD_TO_WGS84, GEO_TO_GSM
+from Lgm_Wrap import TEME_TO_WGS84, WGS84_TO_GSM, GSM_TO_WGS84, WGS84_TO_GSE, GSE_TO_WGS84, SM_TO_GSM, GSM_TO_SM, GSM_TO_GSE, GSE_TO_GSM, MOD_TO_GSM, GSE_TO_SM, SM_TO_GSE, MOD_TO_GSE, MOD_TO_WGS84, GEO_TO_GSM, GSE_TO_GEI2000, GEI2000_TO_GSM, GSM_TO_GEI2000, GEI2000_TO_GSE
 from lgmpy import Lgm_Vector, Lgm_CTrans, Lgm_MagModelInfo
 from lgmpy.Lstar import Lstar_Data
 
@@ -39,7 +39,11 @@ conv_dict = {'SM_GSM': SM_TO_GSM,
                  'SM_GSE' : SM_TO_GSE,
                  'MOD_GSE' : MOD_TO_GSE,
                  'MOD_WGS84' : MOD_TO_WGS84,
-                 'GEO_GSM' : GEO_TO_GSM,}
+                 'GEO_GSM' : GEO_TO_GSM,
+                 'GSE_GEI2000' : GSE_TO_GEI2000,
+                 'GEI2000_GSE' : GEI2000_TO_GSE,
+                 'GEI2000_GSM' : GEI2000_TO_GSM,
+                 'GSM_GEI2000' : GSM_TO_GEI2000,}
 
 def coordTrans(*args):
     '''
