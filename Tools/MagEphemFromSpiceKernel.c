9d8d930f19e56e09271b3ceb084f7ef3094e8c70
Mike Henderson <mghenderson@lanl.gov>
2013-03-08 16:08:39 -0700

    Added T96 option to MagEphemFromSpiceKernel.c

--- Tools/MagEphemFromSpiceKernel.c
+++ Tools/MagEphemFromSpiceKernel.c
@@ -889,6 +889,8 @@ int main( int argc, char *argv[] ){
         OverRideKp = TRUE;
     } else if ( !strcmp( ExtModel, "T89D" ) ){
         MagEphemInfo->LstarInfo->mInfo->Bfield = Lgm_B_T89;
+    } else if ( !strcmp( ExtModel, "T96" ) ){
+        MagEphemInfo->LstarInfo->mInfo->Bfield = Lgm_B_T96;
     } else if ( !strcmp( ExtModel, "TS04D" ) ){
         MagEphemInfo->LstarInfo->mInfo->Bfield = Lgm_B_TS04;
     } else if ( !strcmp( ExtModel, "TS07D" ) ){
