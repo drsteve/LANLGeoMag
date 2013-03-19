1cc6ebfd564f0405cc52aab5eec523b1d085477f
Steve Morley <smorley@lanl.gov>
2013-03-19 12:20:18 -0600

    lgmpy: some basic mods to include T96

--- Python/lgmpy/_Bfield_dict.py
+++ Python/lgmpy/_Bfield_dict.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from Lgm_Wrap import Lgm_Set_Lgm_B_cdip, Lgm_Set_Lgm_B_edip, Lgm_Set_Lgm_B_OP77, Lgm_Set_Lgm_B_T89, Lgm_Set_Lgm_B_Dungey
+from Lgm_Wrap import Lgm_Set_Lgm_B_cdip, Lgm_Set_Lgm_B_edip, Lgm_Set_Lgm_B_OP77, Lgm_Set_Lgm_B_T89, Lgm_Set_Lgm_B_Dungey, Lgm_Set_Lgm_B_T96
+
 
 Bfield_dict = {'Lgm_B_cdip': Lgm_Set_Lgm_B_cdip,
                 'Lgm_B_edip': Lgm_Set_Lgm_B_edip,
                 'Lgm_B_OP77': Lgm_Set_Lgm_B_OP77,
                 'Lgm_B_T89': Lgm_Set_Lgm_B_T89,
+                'Lgm_B_T96': Lgm_Set_Lgm_B_T96,
                 'Lgm_B_Dungey': Lgm_Set_Lgm_B_Dungey}
