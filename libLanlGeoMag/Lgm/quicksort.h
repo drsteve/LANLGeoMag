128e59e930af017aa5826d62c4fbadc71952edee
Michael G. Henderson <mghenderson@lanl.gov>
2023-11-15 09:28:21 -0700

    Added additional prototypes for quicksort routines

--- libLanlGeoMag/Lgm/quicksort.h
+++ libLanlGeoMag/Lgm/quicksort.h
@@ -2,8 +2,11 @@
 #define LGM_QUICKSORT_H
 
 void quicksort( unsigned long n, double *arr );
+void quicksort_uli( unsigned long n, unsigned long *arr );
 void quicksort2(  unsigned long n, double *arr, double *brr );
 void quicksort2uli(  unsigned long n, double *arr, unsigned long int *brr );
+void quicksort3(  unsigned long n, double *arr, double *brr, double *crr );
 void bubbleSort(unsigned long n, double *arr);
+
 
 #endif
