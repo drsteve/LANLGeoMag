607a674747f6a0f185be2cf58ab27aa69996aeee
Michael G. Henderson <mghenderson@lanl.gov>
2023-11-15 09:43:21 -0700

    Added derivatives of the distribution wrt params. Cleaned up comments.

--- libLanlGeoMag/Lgm/Lgm_MaxwellJuttner.h
+++ libLanlGeoMag/Lgm/Lgm_MaxwellJuttner.h
@@ -11,6 +11,7 @@
 #include <gsl/gsl_sf_bessel.h>
 
 double  Lgm_MaxJut( double n, double T, double Ek, double E0 );
+void    Lgm_MaxJut_Derivs( double n, double T, double Ek, double E0, double *dfdn, double *dfdT );
 double  Lgm_Maxwellian( double n, double T, double Ek, double E0 );
 
 
